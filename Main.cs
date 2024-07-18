4538a87a391f15e05efa29c7af6f96ad438416b8
GravityScriptsV2 <95504918+GravityScriptsV2@users.noreply.github.com>
2024-07-18 16:30:03 -0700

    Add project files.

---
+++ Main.cs
@@ -0,0 +1,43 @@
+﻿using GTA;
+using GTA.Math;
+using GTA.Native;
+using System;
+
+namespace NoNPC
+{
+    public class Main : Script
+    {
+        public Main()
+        {
+            Tick += OnTick;
+        }
+
+        private void OnTick(object sender, EventArgs e)
+        {
+            Wait(0);
+
+            // Stop Spawn
+            Function.Call(Hash.SET_CREATE_RANDOM_COPS, false);
+            Function.Call(Hash.SET_CREATE_RANDOM_COPS_NOT_ON_SCENARIOS, false);
+            Function.Call(Hash.SET_CREATE_RANDOM_COPS_ON_SCENARIOS, false);
+            Function.Call(Hash.SET_GARBAGE_TRUCKS, false);
+            Function.Call(Hash.SET_RANDOM_BOATS, false);
+            Function.Call(Hash.SET_VEHICLE_DENSITY_MULTIPLIER_THIS_FRAME, 0.0f);
+            Function.Call(Hash.SET_PED_DENSITY_MULTIPLIER_THIS_FRAME, 0.0f);
+            Function.Call(Hash.SET_RANDOM_VEHICLE_DENSITY_MULTIPLIER_THIS_FRAME, 0.0f);
+            Function.Call(Hash.SET_SCENARIO_PED_DENSITY_MULTIPLIER_THIS_FRAME, 0.0f, 0.0f);
+            Function.Call(Hash.SET_PARKED_VEHICLE_DENSITY_MULTIPLIER_THIS_FRAME, 0.0f);
+
+            // Prevent Firefighters and other Services to spawn
+            for (int i = 1; i <= 12; i++)
+            {
+                Function.Call(Hash.ENABLE_DISPATCH_SERVICE, i, false);
+            }
+
+            // Clear NPC
+            Vector3 coords = Game.Player.Character.Position;
+            Function.Call(Hash.CLEAR_AREA_OF_VEHICLES, coords.X, coords.Y, coords.Z, 1000, false, false, false, false, false);
+            Function.Call(Hash.REMOVE_VEHICLES_FROM_GENERATORS_IN_AREA, coords.X - 500.0, coords.Y - 500.0, coords.Z - 500.0, coords.X + 500.0, coords.Y + 500.0, coords.Z + 500.0);
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("NoNPC")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Venoxity Development")]
+[assembly: AssemblyProduct("NoNPC")]
+[assembly: AssemblyCopyright("Copyright © 2021-2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("ff423ab3-99e2-4945-9e07-81c88644eef1")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
